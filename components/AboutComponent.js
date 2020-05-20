6cb57d974cddba87406174e643f791bf6984c3c1
Dylan <dachaconp@unal.edu.co>
2020-05-19 22:33:44 -0500

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { LEADERS } from '../shared/leaders'
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+      leaders: state.leaders
+    }
+  }
 
 function History() {
     return (
@@ -25,17 +32,11 @@ function renderLeaders({item, index}) {
             title={item.name}
             subtitle={item.description}
             hideChevron={true}
-            leftAvatar={{ source: require('./images/alberto.png')}}
+            leftAvatar={{source: {uri: baseUrl + item.image}}}
         />
     );
 };
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            leaders: LEADERS
-        };
-    }
 
     static navigationOptions = {
         title: 'About'
@@ -48,8 +49,8 @@ class About extends Component {
             <History />
             <Card title="Corporate Leadership">
                 <FlatList 
-                    data={this.state.leaders}
-                    renderItem={renderLeaders}
+                    data={this.props.leaders.leaders}
+                    renderItem={renderLeader}
                     keyExtractor={item => item.id.toString()}
                     />
             </Card>
@@ -58,4 +59,4 @@ class About extends Component {
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
