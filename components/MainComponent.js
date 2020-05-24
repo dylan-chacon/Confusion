c5dedb30bedd6079e215271074b92090f2e4c199
Dylan <dachaconp@unal.edu.co>
2020-05-24 00:13:13 -0500

    forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -68,6 +68,23 @@ const HomeNavigator = createStackNavigator({
         onPress={ () => navigation.toggleDrawer() } />    
     }) 
 });
+
+const ReservationNavigator = createStackNavigator({
+  Reservation: { screen: Reservation }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.navigate('DrawerToggle') } />    
+  })
+})
 
 const ContactNavigator = createStackNavigator({
   Contact: { screen: Contact }
@@ -149,6 +166,21 @@ const MainNavigator = createDrawerNavigator({
             />
           ),
         }, 
+      },
+      Reservation:
+      { screen: ReservationNavigator,
+        navigationOptions: {
+          title: 'Reserve Table',
+          drawerLabel: 'Reserve Table',
+          drawerIcon: ({ tintColor, focused }) => (
+            <Icon
+              name='cutlery'
+              type='font-awesome'            
+              size={24}
+              iconStyle={{ color: tintColor }}
+            />
+          ),
+        }
       },
     Contact: 
       { screen: ContactNavigator,
