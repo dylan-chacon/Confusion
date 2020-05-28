b0a5387f3a93b58fb14f27c3a7f0a28de6ca7cf1
Dylan <dachaconp@unal.edu.co>
2020-05-27 23:06:26 -0500

    Swipeout

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -152,6 +152,11 @@ export const addFavorite = (dishId) => ({
     payload: dishId
 });
 
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+});
+
 export const postComment = (dishId, rating, author, comment)  => (dispatch) => {
     const date = new Date().toISOString;
     const commentPayload = {dishId, rating, author, comment, date};
