0a3d9970f04a9deec99dcecfbf1e3b7762966e13
Dylan <dachaconp@unal.edu.co>
2020-05-28 22:44:59 -0500

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
     constructor(props) {
@@ -13,16 +14,18 @@ class Contact extends Component {
 
     render() {
         return (
-        <Card title="Contact Information">
-                <Text>
-                {`121, Clear Water Bay Road\n\n`}
-                {`Clear Water Bay, Kowloon\n\n`}
-                {`HONG KONG\n\n`}
-                {`Tel: +852 1234 5678\n\n`}
-                {`Fax: +852 8765 4321\n\n`}
-                {`Email:confusion@food.net`}
-                </Text>
-        </Card>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>     
+            <Card title="Contact Information">
+                    <Text>
+                    {`121, Clear Water Bay Road\n\n`}
+                    {`Clear Water Bay, Kowloon\n\n`}
+                    {`HONG KONG\n\n`}
+                    {`Tel: +852 1234 5678\n\n`}
+                    {`Fax: +852 8765 4321\n\n`}
+                    {`Email:confusion@food.net`}
+                    </Text>
+            </Card>
+        </Animatable.View>
         );
     }
 }
