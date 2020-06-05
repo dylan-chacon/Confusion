1cb60a63145b8d1230dd39084b7ae51e94ebbef5
Dylan <dachaconp@unal.edu.co>
2020-06-05 15:28:10 -0500

    image picker

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -140,20 +140,21 @@ const FavoritesNavigator = createStackNavigator({
 })
 
 const LoginNavigator = createStackNavigator({
-    Login: { screen: Login }
+  Login: Login
 }, {
-  navigationOptions: ({ navigation }) => ({
-    headerStyle: {
-        backgroundColor: "#512DA8"
-    },
-    headerTitleStyle: {
-        color: "#fff"            
-    },
-    headerTintColor: "#fff",
-    headerLeft: <Icon name="menu" size={24}
-      iconStyle={{ color: 'white' }} 
-      onPress={ () => navigation.toggleDrawer() } />    
-  })
+navigationOptions: ({ navigation }) => ({
+  headerStyle: {
+      backgroundColor: "#512DA8"
+  },
+  headerTitleStyle: {
+      color: "#fff"            
+  },
+  title: 'Login',
+  headerTintColor: "#fff",
+  headerLeft: <Icon name="menu" size={24}
+    iconStyle={{ color: 'white' }} 
+    onPress={ () => navigation.toggleDrawer() } />    
+})
 });
 
 const CustomDrawerContentComponent = (props) => (
@@ -171,6 +172,7 @@ const CustomDrawerContentComponent = (props) => (
     </SafeAreaView>
   </ScrollView>
 );
+
 
 const MainNavigator = createDrawerNavigator({
     Login: 
